adea9ed5447c089f40a6f24758ed02cc84734fc9
Vickie Groteboer <vickie.groteboer@gmail.com>
2016-07-13 21:15:52 -0400

    Added CODING-TEST-2-1.py file

---
+++ PYTHON-FILES/CODING-TEST-2-1.py
@@ -0,0 +1,17 @@
+import string
+
+def first_half (s):
+   ret_value=''
+
+   char_num = len(s)
+
+   if char_num %2 == 0:
+       first_half_length = char_num/2
+   else :
+       first_half_length = ((char_num/2)+1)
+
+   return (s[ 0 : first_half_length]) 
+
+imp = raw_input('Wnter the string here: ')
+print first_half(imp)
+
